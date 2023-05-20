7e9996877b56ce77863afb05580c2f3b5dab0fc1
Gagik86 <gagik.harutyunyan8608@gmail.com>
2023-05-20 19:39:25 +0400

    add pause play to video

---
+++ index.js
@@ -0,0 +1,9 @@
+let pause = document.getElementById("pause");
+let video = document.getElementById("video");
+
+function boo() {
+    if(video.paused) {
+        video.play();
+    } else(video.pause())
+}
+
