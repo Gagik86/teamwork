1aa4bb1554d497fcf2e86f454df6d82820d68c21
=MesropGhukasyan <=mesropghukasyan@gmail.com>
2023-05-23 02:47:59 +0400

    added popup without transition

--- index.js
+++ index.js
@@ -1,16 +1,26 @@
-let pause = document.getElementById("pause");
 let video = document.getElementById("video");
 
 
 function boo() {
-    if(video.paused) {
-        document.getElementById("fa-play").style.display="none";
-        document.getElementById("pause").style.display="block";
+    let playIcon = document.getElementById("playPause");
+    if (playIcon.classList.contains('fa-play')) {
+        playIcon.classList.remove('fa-play');
+        playIcon.classList.add('fa-pause');
         video.play();
     } else {
-        video.pause()
-        document.getElementById("fa-play").style.display="block";
-        document.getElementById("pause").style.display="none";
+        playIcon.classList.remove('fa-pause');
+        playIcon.classList.add('fa-play');
+        video.pause();
     }
+
 }
 
+function openPopup() {
+    let myElement=document.getElementsByClassName("popup")[0];
+    myElement.style.visibility="visible";
+}
+
+ function closePopup(){
+    let myElement=document.getElementsByClassName("popup")[0];
+    myElement.style.visibility="hidden";
+}
